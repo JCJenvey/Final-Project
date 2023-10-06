0e1c5c465c753b34abe2bb388e2a0658be7aac73
Jared Jenvey <jared@exit-12.com>
2023-10-06 04:02:08 +0000

    Finished issue#1

---
+++ client/src/components/AddBookForm.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import './AddBookForm.css';
+
+export default function AddBookForm({ onSubmit }) {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [summary, setSummary] = useState('');
+  const [enjoyed, setEnjoyed] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newBook = {
+      title,
+      author,
+      summary,
+      enjoyed,
+    };
+    onSubmit(newBook);
+    setTitle('');
+    setAuthor('');
+    setSummary('');
+    setEnjoyed(false);
+  }
+
+  return (
+    <form className="input-group mb-4 shadow-sm col" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Book Title</label>
+        <input
+          required
+          autoFocus
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="form-control"
+          id="title"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="author">The Author</label>
+        <input
+          required
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          className="form-control"
+          id="author"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="summary">Summary (optional)</label>
+        <textarea
+          onChange={(e) => setSummary(e.target.value)}
+          className="form-control"
+          id="summary"
+          value={summary}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="enjoyed">Did you enjoy it?</label>
+        <input
+          type="checkbox"
+          checked={enjoyed}
+          onChange={(e) => setEnjoyed(e.target.checked)}
+          className="form-control"
+          id="enjoyed"
+        />
+      </div>
+
+      <div className="input-group-append">
+        <button type="submit" className="btn btn-primary">
+          Add Book
+        </button>
+      </div>
+    </form>
+  );
+}
---
+++ client/src/components/Books.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import AddBookForm from './AddBookForm';
+import PageTitle from './PageTitle';
+
+export default function Books() {
+  const [books, setBooks] = useState([]);
+  const [error, setError] = useState();
+
+  async function addBook(newBook) {
+    try {
+      console.log(newBook);
+      const response = await fetch(`/api/books`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newBook),
+      });
+
+      const statusCode = response.status;
+      if (!response.ok) {
+        throw new Error(statusCode);
+      }
+      const result = await response.json();
+      setBooks(books.concat(result));
+    } catch (err) {
+      setError(err);
+    }
+  }
+
+  if (error) {
+    console.error('Fetch error:', error);
+    return <div>Error! {error.message}</div>;
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col pt-5">
+          <PageTitle text="Book Journal App" />
+          <AddBookForm onSubmit={addBook} />
+        </div>
+      </div>
+    </div>
+  );
+}
